ecf7d30c78a6e6ac9f33632378f2d84960aca4a3
erdevendrapsingh-lang <er.devendrapsingh@gmail.com>
2026-09-14 17:35:42 +0530

    Change default VM size for AKS nodes

--- variables.tf
+++ variables.tf
@@ -31,5 +31,5 @@ variable "node_count" {
 variable "vm_size" {
   description = "VM size for AKS nodes"
   type        = string
-  default     = "Standard_B2s"
+  default     = "Standard_B2als_v2"
 }
